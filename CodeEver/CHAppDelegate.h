7b500eaeeb36b3c3c4f02e988335c8de457d301e
Hang Chen <hangisnice@gmail.com>
2014-01-03 13:41:47 +0800

    Initial commit
    Initial commit

---
+++ CodeEver/CHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHAppDelegate.h
+//  CodeEver
+//
+//  Created by hangchen on 1/3/14.
+//  Copyright (c) 2014 hangchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CodeEver/CHViewController.m
@@ -0,0 +1,32 @@
+//
+//  CHViewController.m
+//  CodeEver
+//
+//  Created by hangchen on 1/3/14.
+//  Copyright (c) 2014 hangchen. All rights reserved.
+//
+
+#import "CHViewController.h"
+#import "SDWebImage/UIImageView+WebCache.h"
+#import "AFNetworking.h"
+
+@interface CHViewController ()
+
+@end
+
+@implementation CHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
